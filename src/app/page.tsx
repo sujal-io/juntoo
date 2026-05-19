bf5a7ad0c4d776f7f3ad8b9a711ca249f65f697d
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-05-20 02:01:47 +0530

    Set up Clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+"use client";
+
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello home page</h1>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+        <UserButton />
+      </header>
     </div>
   );
 }
